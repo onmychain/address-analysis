f55199e813a17c1efacc471803d14ba7bd15b2ec
Cyrille Derche <c.derche@me.com>
2023-04-05 14:22:54 +0200

    Should verify valid address

--- supabase/functions/address/index.ts
+++ supabase/functions/address/index.ts
@@ -2,24 +2,31 @@
 // https://deno.land/manual/getting_started/setup_your_environment
 // This enables autocomplete, go to definition, etc.
 
-import { serve } from "https://deno.land/std@0.168.0/http/server.ts"
+import { serve } from "server"
+import { ethers } from "ethers"
+import { Status } from "http_status"
+import { respond } from "../_shared/respond.ts"
 
-console.log("Hello from Functions!")
+serve(async (req: Request) => {
+  const { address } = await req.json()
 
-serve(async (req) => {
-  const { name } = await req.json()
-  const data = {
-    message: `Hello ${name}!`,
+  if (!address) {
+    const data = { message: 'address is missing.' }
+    return respond(data, Status.BadRequest)
   }
 
-  return new Response(
-    JSON.stringify(data),
-    { headers: { "Content-Type": "application/json" } },
-  )
+  if (!ethers.utils.isAddress(address)) {
+    const data = { message: `The address ${address} is invalid.` }
+    return respond(data, Status.BadRequest)
+  }
+
+  const data = { message: `The address ${address} is valid!` }
+  return respond(data, Status.OK)
+
 })
 
 // To invoke:
 // curl -i --location --request POST 'http://localhost:54321/functions/v1/address' \
 //   --header 'Authorization: Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZS1kZW1vIiwicm9sZSI6ImFub24iLCJleHAiOjE5ODM4MTI5OTZ9.CRXP1A7WOeoJeXxjNni43kdQwgnWNReilDMblYTn_I0' \
 //   --header 'Content-Type: application/json' \
-//   --data '{"name":"Functions"}'
+//   --data '{"address":"0x8ba1f109551bd432803012645ac136ddd64dba72"}'
