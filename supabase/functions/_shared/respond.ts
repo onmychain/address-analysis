4b1eed5ad69ca05d0ecda197465b5f6d94fe0853
Cyrille Derche <c.derche@me.com>
2023-04-11 09:34:50 +0200

    remove all

--- supabase/functions/_shared/respond.ts
+++
@@ -1,7 +0,0 @@
-const opts = {
-    'Content-Type': 'application/json'
-}
-
-export function respond (data: any, status: number) {
-    return new Response(JSON.stringify(data), { ...opts, status })
-}
